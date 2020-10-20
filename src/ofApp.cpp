5495176c705f57e97c16c4aa41665f4ca21c062d
Brian-La <71418970+Brian-La@users.noreply.github.com>
2020-10-19 23:08:35 -0700

    update 3D
    testing 3D applications using ofCam!

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,77 +1,130 @@
 #include "ofApp.h"
 
 
-glm::vec3 dragPos, mouPos, diffPos;     //drag, mouse, and difference position vectors
-bool dragSwitch1, dragSwitch2;        //bool switches for object drag
-
 //constructor(default)------------------------------------------
-Rectangle::Rectangle() {
-    pos = glm::vec3(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()), 0);     //set default position randomly within window
+RectShape::RectShape(bool x) {
+    pos = glm::vec3(ofRandom(ofGetWidth() / 2), ofRandom(ofGetHeight() / 2), 0);     //set default position randomly within window
+    
+    color = (x) ? ofColor::red : ofColor::blue;         //sys1 = red, sys2 = blue
 }
 
 //constructor(position x, position y, speed of rotation)----------------
-Rectangle::Rectangle(float s, bool h) {
+RectShape::RectShape(float s, bool x) {
     //set variables
     pos = glm::vec3(0, 0, 0);       //default 0
     speed = s;
     
     //width/height
-    width = 20;
-    height = (h) ? 50 : 100;    //denote height based on bool param. (1 = 50, 0 = 100)
+    width = (x) ? 50 : 100;
+    height = 20;    //denote height based on bool param. (1 = 50, 0 = 100)
     
-    color = ofColor::blue;      //default blue
+    color = ofColor::yellow;      //default yellow
 }
 
+//constructor(set width/height of each box primitive depending on )
+Rects3D::Rects3D(RectShape a, RectShape a1, RectShape a2) {
+    mainColor = a.color;
+    aWidth = a.width;
+    aHeight = a.height;
+    
+    outerColor = a1.color;
+    a1Width = a1.width;
+    a1Height = a1.height;
+    a1speed = a1.speed;
+    
+    a2Width = a2.width;
+    a2Height = a2.height;
+    a2speed = a2.speed;
+}
 
+//node for system transformations
+ofNode& Rects3D::getNode() {
+    return b;
+}
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetBackgroundColor(ofColor::black);
     
-    r1a = Rectangle(2, 0);        //set rect. to default 0 for translation
-    r1b = Rectangle(5, 1);        //...
+    //adjust r2 speed
+    r2.speed = 2;
     
-    r2.speed = 3;         //set to faster rotation for second rectangle
-    r2a = Rectangle(7, 0);
-    r2b = Rectangle(1, 1);
+    //setup rect3D systems========================================================]]]
+    sys1.setup();
+    sys2.setup();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //change colors based on drag switch
+    r1.color = (dragSwitch1) ? ofColor::green : ofColor::red;
+    r2.color = (dragSwitch2) ? ofColor::green : ofColor::blue;
+    
+    //update deg rotations
+    r1.update();
+    r1a.update();
+    r1b.update();
+    
+    r2.update();
+    r2a.update();
+    r2b.update();
+    
+    //update sys and positions========================================================]]]
+    n1.setPosition(r1.pos.x, 0, r1.pos.y);      //node shift entire position
+    n1.rotateDeg(-r1.speed, {0, 1, 0});      //rotate entire position based on speed
+    sys1.update();
+    
+    n2.setPosition(r2.pos.x, 0, r2.pos.y);      //node shift entire position
+    n2.rotateDeg(-r2.speed, {0, 1, 0});      //rotate entire position based on speed
+    sys2.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    //change colors based on drag switch
-    r1.color = (dragSwitch1) ? ofColor::green : ofColor::orange;
-    r2.color = (dragSwitch2) ? ofColor::green : ofColor::yellow;
     
-    r1.draw();
-    r2.draw();
-    
-    ofPushMatrix();
-    ofTranslate(r1.pos);       //translate based on big rect. position
-    ofRotateZDeg(r1.deg);    //rotate at same pace as big rect.
-    r1a.pos.y = -r1.height/2;     //adjust rect. positions after translation
-    r1b.pos.y = r1.height/2;
-    r1a.draw();
-    r1b.draw();
-    ofPopMatrix();
-    
-    //repeat for rect 2
-    ofPushMatrix();
-    ofTranslate(r2.pos);
-    ofRotateZDeg(r2.deg);
-    r2a.pos.y = -r2.height/2;
-    r2b.pos.y = r2.height/2;
-    r2a.draw();
-    r2b.draw();
-    ofPopMatrix();
+    if(!camMode) {
+        r1.draw();
+        r2.draw();
+        
+        ofPushMatrix();
+        ofTranslate(r1.pos);       //translate based on big rect. position
+        ofRotateZDeg(r1.deg);    //rotate at same pace as big rect.
+        r1a.pos.x = -r1.width/2;     //adjust rect. positions after translation
+        r1b.pos.x = r1.width/2;
+        r1a.draw();
+        r1b.draw();
+        ofPopMatrix();
+        
+        //repeat for rect 2
+        ofPushMatrix();
+        ofTranslate(r2.pos);
+        ofRotateZDeg(r2.deg);
+        r2a.pos.x = -r2.width/2;
+        r2b.pos.x = r2.width/2;
+        r2a.draw();
+        r2b.draw();
+        ofPopMatrix();
+    }
+    else {
+        cam.begin();
+        //sys draw=================================================================]]
+        sys1.draw();
+        sys2.draw();
+        cam.end();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if(key == 'm' || key == 'M') {
+        camMode = !camMode;     //toggle camera
+        
+        if(camMode) {       //reset cam position if entering cam mode
+            cam.setPosition ({ 500, 500, 1000 });
+            cam.lookAt({500, 0, 500}, {0, 1, 0});
+            cam.setFarClip(10000);
+        }
+    }
 }
 
 //--------------------------------------------------------------
@@ -86,26 +139,29 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-    dragPos = glm::vec3(x, y, 0);
-    if(dragSwitch1)
-        r1.pos = dragPos - diffPos;
-    if(dragSwitch2)
-        r2.pos = dragPos - diffPos;
+    if(!camMode) {
+        dragPos = glm::vec3(x, y, 0);
+        if(dragSwitch1)
+            r1.pos = dragPos - diffPos;
+        if(dragSwitch2)
+            r2.pos = dragPos - diffPos;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    mouPos = glm::vec3(x, y, 0);       //save mouse position on click
-    if(r1.near(mouPos)) {
-        diffPos = mouPos - r1.pos;      //if within bounds, calculate and save difference
-        dragSwitch1 = true;      //flip drag switch on
+    if(!camMode) {
+        mouPos = glm::vec3(x, y, 0);       //save mouse position on click
+        if(r1.near(mouPos)) {
+            diffPos = mouPos - r1.pos;      //if within bounds, calculate and save difference
+            dragSwitch1 = true;      //flip drag switch on
 
+        }
+        else if(r2.near(mouPos)) {
+            diffPos = mouPos - r2.pos;
+            dragSwitch2 = true;
+        }
     }
-    else if(r2.near(mouPos)) {
-        diffPos = mouPos - r2.pos;
-        dragSwitch2 = true;
-    }
-    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,11 +2,11 @@
 
 #include "ofMain.h"
 
-class Rectangle {
+class RectShape {
 public:
-    //constructors---------s-------------------------------
-    Rectangle();
-    Rectangle(float, bool);
+    //constructors-----------------------------------------
+    RectShape(bool);
+    RectShape(float, bool);
     
     //functions--------------------------------------------
     virtual void draw() {
@@ -16,12 +16,15 @@ public:
         ofTranslate(pos);       //translate coordinate system to default position vector
         ofRotateZDeg(deg);
         
-        deg = deg + speed;         //increment deg based on speed & rotate
-        if(deg > 360)  deg = speed;   //inline-if rotation reaches 359 deg, reset to initial deg
         ofDrawRectangle(-width/2, -height/2, width, height);    //adjust rectangle to origin
         ofPopMatrix();          //pop matrix stack, reset coordinate system
         
     }       //override draw
+    
+    virtual void update() {
+        deg += speed;         //increment deg based on speed & rotate
+        if(deg > 360)  deg = speed;   //inline-if rotation reaches 359 deg, reset to initial deg
+    }
     
     virtual bool near(glm::vec3 p) {
         if(p.x <= pos.x + height && p.x >= pos.x - height &&
@@ -34,13 +37,72 @@ public:
     //data-------------------------------------------------
     glm::vec3 pos;      //position vector of rectangle
     
-    float width = 100;      //rectangle width
-    float height = 200;    //...height
-    float deg;          //degrees of rotation
+    float width = 200;      //rectangle width
+    float height = 50;    //...height
+    float deg = 0;          //degrees of rotation
     float speed = 1;         //speed of rotation specified in parameter
     ofColor color;      //color of rectangle
     
 };
+
+
+class Rects3D {
+public:
+    Rects3D(RectShape, RectShape, RectShape);    //constructor using 3 rectangles
+    
+    //functions---------------------------------------------
+    void setup() {
+        //material.setDiffuseColor(ofFloatColor::turquoise);      //set diffuse color
+        
+        //main box
+        b.setWidth(aWidth);
+        b.setHeight(aHeight);
+        b.setDepth(depth);
+        
+        //outer boxes - affix transformations to main box
+        b1.setParent(b);
+        b1.setWidth(a1Width);
+        b1.setHeight(a1Height);
+        b1.setDepth(depth / 4.0);
+        b1.move(-b.getWidth() / 2.0, b.getHeight() / 2.0, 0);       //affix boxes on main box
+        
+        b2.setParent(b);
+        b2.setWidth(a2Width);
+        b2.setHeight(a2Height);
+        b2.setDepth(depth / 4.0);
+        b2.move(b.getWidth() / 2.0, b.getHeight() / 2.0, 0);
+    }
+    void update() {
+        //rotate outer boxes at a diff speed
+        b1.rotateDeg(-a1speed, {0, 1, 0});
+        b2.rotateDeg(-a2speed, {0, 1, 0});
+    }
+    void draw() {
+        //material.begin();
+        
+        //draw boxes
+        ofSetColor(mainColor);
+        b.draw();
+        ofSetColor(outerColor);
+        b1.draw();
+        b2.draw();
+
+        //material.end();
+    }
+    ofNode& getNode();          //return node of system
+    
+    ofBoxPrimitive b, b1, b2;       //box primitives
+    ofMaterial material;        //keep material color
+    
+    float aWidth, aHeight, a1Width, a1Height, a2Width, a2Height;       //height/width
+    float a1speed = 0;           //speed of rotation for smaller boxes
+    float a2speed = 0;
+    float depth = 100;        //depth of boxes
+    
+    ofColor mainColor, outerColor;
+};
+
+
 
 class ofApp : public ofBaseApp{
 
@@ -61,5 +123,21 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-    Rectangle r1, r1a, r1b, r2, r2a, r2b;      //rectangle calls
+    glm::vec3 dragPos, mouPos, diffPos;     //drag, mouse, and difference position vectors
+    bool dragSwitch1, dragSwitch2, camMode;        //bool switches for object drag, camera mode toggle
+    
+    RectShape r1 = RectShape(true);
+    RectShape r2 = RectShape(false);
+    RectShape r1a = RectShape(2, 0);
+    RectShape r1b = RectShape(5, 1);
+    RectShape r2a = RectShape(7, 0);
+    RectShape r2b = RectShape(1, 1);      //rectangle calls
+    
+    Rects3D sys1 = Rects3D(r1, r1a, r2b);
+    Rects3D sys2 = Rects3D(r2, r2a, r2b);
+    
+    ofNode& n1 = sys1.getNode();
+    ofNode& n2 = sys2.getNode();
+    
+    ofEasyCam cam;
 };
